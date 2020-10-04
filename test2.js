339ea7148c57270b0a1e5dc9e6ca9d28fbf62957
PS Subramanya Bhat <subramanya@robosoftin.com>
2020-10-04 18:22:29 +0530

    Added an iterable prg

--- test2.js
+++ test2.js
@@ -160,3 +160,28 @@ function shuffle2(array) {
 let arr10 = [1, 2, 3];
 shuffle2(arr10);
 console.log(arr10);
+
+
+
+//Iterables
+let range = {
+    from: 1,
+    to: 5,
+  
+    [Symbol.iterator]() {
+        this.current = this.from;
+        return this;
+    },
+  
+    next() {
+        if (this.current <= this.to) {
+            return { done: false, value: this.current++ };
+        } else {
+            return { done: true };
+        }
+    }
+};
+  
+for (let num of range) {
+    console.log(num); // 1, then 2, 3, 4, 5
+}
