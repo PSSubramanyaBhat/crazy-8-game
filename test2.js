6793ab2bf20735f620ed9fee24de5785ee839f8a
PS Subramanya Bhat <subramanya@robosoftin.com>
2020-10-04 18:51:32 +0530

    Added snippets for iterable concept

--- test2.js
+++ test2.js
@@ -185,3 +185,8 @@ let range = {
 for (let num of range) {
     console.log(num); // 1, then 2, 3, 4, 5
 }
+
+for (let char of "test") {
+    // triggers 4 times: once for each character
+    console.log( char ); // t, then e, then s, then t
+}
